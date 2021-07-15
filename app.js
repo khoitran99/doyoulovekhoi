a954dcb0452672fd28340a62aa59adc97ed3d461
khoitran <khoitvhe130007@gmail.com>
2021-07-15 21:01:19 +0700

    third commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var b = document.getElementById("no");
 b.addEventListener("click",change);
 function change()
 {
-    var i = Math.floor(Math.random()*1500)+1;
+    var i = + Math.floor(Math.random()*300)+600;
     var j = Math.floor(Math.random()*500)+1;
     b.style.left = i+"px";
     b.style.top = j+"px";
