5057e77602ab524fc85987c951deffade0a17938
khoitran <khoitvhe130007@gmail.com>
2021-07-15 20:54:45 +0700

    second commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var b = document.getElementById("no");
+b.addEventListener("click",change);
+function change()
+{
+    var i = Math.floor(Math.random()*1500)+1;
+    var j = Math.floor(Math.random()*500)+1;
+    b.style.left = i+"px";
+    b.style.top = j+"px";
+}
+
+var a = document.getElementById("yes");
+a.addEventListener("click",changeText);
+function changeText()
+{
+    document.getElementById("text").innerHTML = "Dear, I love U too <3";
+}
